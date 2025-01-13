5c8353d8c7864690510f9f9b3e435cbf1c6f14ab
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2025-01-13 11:56:11 +0900

    Add sumList()
    Delete build

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,20 @@
 module Main (main) where
 
+import Lib (maxInList, sumList)
 import Test.Hspec
-import Lib (maxInList)
 
 main :: IO ()
 main = hspec $ do
-    describe "maxInList" $ do
-        it "returns Nothing for an empty list" $ do
-            (maxInList ([] :: [Int])) `shouldBe` Nothing
-            
-        it "returns the maximum element for a non-empty list" $ do
-            (maxInList ([1, 2, 3] :: [Int])) `shouldBe` Just 3
+  describe "maxInList" $ do
+    it "returns Nothing for an empty list" $ do
+      (maxInList ([] :: [Int])) `shouldBe` Nothing
+
+    it "returns the maximum element for a non-empty list" $ do
+      (maxInList ([1, 2, 3] :: [Int])) `shouldBe` Just 3
+
+  describe "sumList" $ do
+    it "returns 0 for an empty list" $ do
+      (sumList ([] :: [Int])) `shouldBe` 0
+
+    it "returns the sum of all elements for a non-empty list" $ do
+      (sumList ([1, 2, 3] :: [Int])) `shouldBe` 6
