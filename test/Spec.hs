b305a26c273056d1fac74755fe551afc05c56567
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2025-01-13 16:01:24 +0900

    Fix linter

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,14 +7,14 @@ main :: IO ()
 main = hspec $ do
   describe "maxInList" $ do
     it "returns Nothing for an empty list" $ do
-      (maxInList ([] :: [Int])) `shouldBe` Nothing
+      maxInList ([] :: [Int]) `shouldBe` Nothing
 
     it "returns the maximum element for a non-empty list" $ do
-      (maxInList ([1, 2, 3] :: [Int])) `shouldBe` Just 3
+      maxInList ([1, 2, 3] :: [Int]) `shouldBe` Just 3
 
   describe "sumList" $ do
     it "returns 0 for an empty list" $ do
-      (sumList ([] :: [Int])) `shouldBe` 0
+      sumList ([] :: [Int]) `shouldBe` 0
 
     it "returns the sum of all elements for a non-empty list" $ do
-      (sumList ([1, 2, 3] :: [Int])) `shouldBe` 6
+      sumList ([1, 2, 3] :: [Int]) `shouldBe` 6
